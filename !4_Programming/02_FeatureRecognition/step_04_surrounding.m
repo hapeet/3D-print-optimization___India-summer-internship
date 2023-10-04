ed8caae54b37e939219bbc558d6c9da1cc56a435
hapeet <129796757+hapeet@users.noreply.github.com>
2023-10-04 21:10:00 +0530

    secondCommit

--- !4_Programming/02_FeatureRecognition/step_04_surrounding.m
+++ !4_Programming/02_FeatureRecognition/step_04_surrounding.m
@@ -1,14 +1,37 @@
 clc; close all
 
 
+surroundingWidht = 10/resolution;   % surrounding offset in mm divided by resolution in mm/px > px
+
 for iFeature = 1:numel(propsFeatures)
 
 % CUT X 
-    
-    
-    propsFeatures(iFeature).cutXminus = 
-    
-    
 
+CutX = propsFeatures(iFeature).CutX;
+
+tightCutBoundaries(1) = CutX.CutBoundaries(1) - CutX.CutBoundariesExt(1);
+tightCutBoundaries(2) = CutX.CutBoundaries(2) - CutX.CutBoundariesExt(1)+1;
+
+negSurroundings = zeros(1,surroundingWidht,size(CutX.CutTightWide,3));
+posSurroundings = zeros(1,surroundingWidht,size(CutX.CutTightWide,3));
+
+negSurroundings(find(CutX.CutTightWide(1,1:tightCutBoundaries(1),:))) = 1;
+posSurroundings(find(CutX.CutTightWide(1,tightCutBoundaries(2)+1:end,:))) = 1;
+
+
+figure
+subplot(2,2,[1,3])
+imshow(squeeze(CutX.CutTightWide)) 
+axis on
+
+subplot(2,2,2)
+imshow(squeeze(negSurroundings))
+axis on
+
+subplot(2,2,4)
+imshow(squeeze(posSurroundings))
+axis on
+
+close all
 
 end
